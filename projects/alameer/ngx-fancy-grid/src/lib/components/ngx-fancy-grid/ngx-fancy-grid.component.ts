d9cbad092d2e19063f6dc82839c4c8a52f3c435b
alameerashraf <alamiir.ashraf@gmail.com>
2022-01-10 02:03:33 +0200

    Bind data onChnages to the grid insider

--- projects/alameer/ngx-fancy-grid/src/lib/components/ngx-fancy-grid/ngx-fancy-grid.component.ts
+++ projects/alameer/ngx-fancy-grid/src/lib/components/ngx-fancy-grid/ngx-fancy-grid.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { constants } from '../../modules/constants/constants';
+import { NGXheaders } from '../../NGXheaders';
 
 @Component({
   selector: 'ngx-fancy-grid',
@@ -8,11 +9,17 @@ import { constants } from '../../modules/constants/constants';
 })
 export class NgxFancyGridComponent implements OnChanges {
   @Input('design') designClass: string = "";
+  @Input('headers') tableHeaders: NGXheaders[] = [];
+  @Input('data') data: any[] = [];
   gridStyle = "";
+  gridHeaders: NGXheaders[] = [];
+  gridData: any[] = [];
 
   constructor() { }
 
   ngOnChanges(): void {
-    this.gridStyle = constants.GRID_STYLE[this.designClass]
+    this.gridStyle = constants.GRID_STYLE[this.designClass];
+    this.gridHeaders = this.tableHeaders;
+    this.gridData = this.data;
   }
 }
